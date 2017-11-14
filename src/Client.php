6a847307475c12b248746eec8f6142918a5efea9
tanelt <taneltahepold@gmail.com>
2017-11-14 11:38:09 +0200

    Composer and test setup

---
+++ src/Client.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by tanel @14.11.17 11:13
+ */
+
+namespace PDFGeneratorAPI;
+
+
+class Client
+{
+    public function __construct($token, $secret)
+    {
+    }
+}
---
+++ tests/Client.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by tanel @14.11.17 11:23
+ */
+
+namespace PDFGeneratorAPI\Tests;
+
+class Client extends \PHPUnit_Framework_TestCase
+{
+    public function testMain()
+    {
+        echo 1;
+    }
+}
