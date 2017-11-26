d768ad310e506066bddc6a31aa885202c1509bf5
tanelt <taneltahepold@gmail.com>
2017-11-26 10:15:28 +0200

    Updated api sematics. Use key instead of token

--- tests/Client.php
+++ tests/Client.php
@@ -13,7 +13,7 @@ class Client extends \PHPUnit_Framework_TestCase
      * @var \ActualReports\PDFGeneratorAPI\Client
      */
     protected $client;
-    protected $token = '61e5f04ca1794253ed17e6bb986c1702';
+    protected $key = '61e5f04ca1794253ed17e6bb986c1702';
     protected $secret = '68db1902ad1bb26d34b3f597488b9b28';
     protected $workspace = 'demo.example@actualreports.com';
     protected $host = 'http://127.0.0.3';// 'https://staging.pdfgeneratorapi.com';
@@ -23,7 +23,7 @@ class Client extends \PHPUnit_Framework_TestCase
     {
         parent::setUp();
 
-        $this->client = new \ActualReports\PDFGeneratorAPI\Client($this->token, $this->secret, $this->workspace);
+        $this->client = new \ActualReports\PDFGeneratorAPI\Client($this->key, $this->secret, $this->workspace);
         $this->client->setBaseUrl($this->host.'/api/v3');
     }
 
@@ -68,14 +68,14 @@ class Client extends \PHPUnit_Framework_TestCase
     public function testEditorDataUrl()
     {
         $url = $this->client->editor($this->templateId, $this->host.'/assets/web/data/qbo_invoice.json');
-        $compareUrl = $this->host.'/api/v3/templates/21650/editor?token=61e5f04ca1794253ed17e6bb986c1702&workspace=demo.example%40actualreports.com&signature=f119b90f5a0a10b09f735be6f7b46d27c02b82b03b31097f577fc0fba7e617b2&data=http%3A%2F%2F127.0.0.3%2Fassets%2Fweb%2Fdata%2Fqbo_invoice.json';
+        $compareUrl = $this->host.'/api/v3/templates/21650/editor?key=61e5f04ca1794253ed17e6bb986c1702&workspace=demo.example%40actualreports.com&signature=1887f3df76acee7965758160f1baf39ce271e804cbf98f1677ae7a90716a31a8&data=http%3A%2F%2F127.0.0.3%2Fassets%2Fweb%2Fdata%2Fqbo_invoice.json';
         $this->assertEquals($compareUrl, $url);
     }
 
     public function testEditorDataArray()
     {
         $url = $this->client->editor($this->templateId, ['DocNumber' => 1123123123]);
-        $compareUrl = $this->host.'/api/v3/templates/21650/editor?token=61e5f04ca1794253ed17e6bb986c1702&workspace=demo.example%40actualreports.com&signature=f119b90f5a0a10b09f735be6f7b46d27c02b82b03b31097f577fc0fba7e617b2&data=%7B%22DocNumber%22%3A1123123123%7D';
+        $compareUrl = $this->host.'/api/v3/templates/21650/editor?key=61e5f04ca1794253ed17e6bb986c1702&workspace=demo.example%40actualreports.com&signature=1887f3df76acee7965758160f1baf39ce271e804cbf98f1677ae7a90716a31a8&data=%7B%22DocNumber%22%3A1123123123%7D';
         $this->assertEquals($compareUrl, $url);
     }
 
